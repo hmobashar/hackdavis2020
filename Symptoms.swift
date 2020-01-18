24edea032725320628acc85002284de34f3274ac
Yiping Su <ypsu@ucdavis.edu>
2020-01-18 15:17:20 -0800

    Add Symptom Class
    * Currently located in main directory

---
+++ Symptoms.swift
@@ -0,0 +1,13 @@
+import Firebase
+
+class Symptom {
+
+    var name: String
+    var occurred: Bool
+
+    init(name: String) {
+        self.name = name
+        self.occurred = false
+    }
+    
+} // Symptoms: Defines a user's symptom
