44b5141f5dafe7dabb460003dd63857dbb7ceb4d
Yiping Su <ypsu@ucdavis.edu>
2020-01-18 23:41:02 -0800

    Add Database Test Funcs in ViewController

--- Tend/Tend/ViewController.swift
+++ Tend/Tend/ViewController.swift
@@ -38,6 +38,14 @@ class ViewController: UIViewController {
         // numberOfDownloadsDataEntries = [iosDataEntry, macDataEntry]
         
         // updateChartData()
+
+
+
+        // FIXME: Test Functions for Firebase
+        testWriteDatabase()
+        testReadDatabase()
+
+
     }
     
     // @IBAction func changeiOS(_ sender: UIStepper) {
@@ -63,6 +71,61 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+
+
+    // TODO: TEST Write Database
+    func testWriteDatabase() {
+
+        // TODO: Test Medication
+        let testMed = Medication(name: "Prozac", dosage: 103.56)
+        uploadMedication(testMed)
+        logMedication(med: testMed, date: "01/18/2020")
+
+        // TODO: Test Symptom
+        let testSymptom = Symptom(name: "tummy-ache", occur: true)
+        uploadSymptom(symptom: testSymptom)
+        logSymptom(sym: testSymptom, date: "01/18/2020")
+
+        // TODO: Test Meal Logging
+        var medArr = [Medication]()
+        let med1 = Medication(name: "Laugh-gas", dosage: 23.4)
+        let med2 = Medication(name: "Mean Girls", dosage: 0.0)
+        medArr.append(med1)
+        medArr.append(med2)
+        var symArr = [Symptom]()
+        let sym1 = Symptom(name: "Hangry", occur: true)
+        symArr.append(sym1)
+        let testMeal = Meal(name: "Pasta", med: medArr, sym: symArr,
+            description: "Pasta was too good for me!")
+        logMeal(testMeal, "01/18/2020")
+
+    }
+
+
+
+    // TODO: Test Read Database
+    func testReadDatabase() {
+        
+        // test Medication
+        var med = getMedication("01/18/2020")
+        for med_item in med {
+            print(med)
+        }
+
+        // test Symptom
+        var sym = getSymptom("01/18/2020")
+        for sym_item in sym {
+            print(sym_item)
+        }
+
+        // test Meal
+        var meal = getMeal("01/18/2020")
+        for meal_item in meal {
+            print(meal_item)
+        }
+
     }
 
 }
