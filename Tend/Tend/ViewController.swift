a2b3274523a88a25443253b348fac98ff7b7ed50
Hajra Mobashar <hmobashar@ucdavis.edu>
2020-01-19 04:44:53 -0800

    bio info logs now makes a pop up

--- Tend/Tend/ViewController.swift
+++ Tend/Tend/ViewController.swift
@@ -13,10 +13,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var barView: BarChartView!
     @IBOutlet weak var loadingImage: UIImageView!
+    @IBOutlet weak var dailyLog: UIView!
+    @IBOutlet weak var logView: UIView!
     
-    var data = ["Morning", "Afternoon", "Evening"]
-    var vals = [0.5,2.0,1.0]
     let formato:BarChartFormatter = BarChartFormatter()
+    var data = ["Morning", "Afternoon", "Evening"]
+       var vals = [0.5,2.0,1.0]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +29,7 @@ class ViewController: UIViewController {
         barView.xAxis.labelCount = 3
         barView.legend.enabled = false
         setChart(dataPoints: data, values: vals)
+        logView.isHidden = true
     }
     
     
@@ -35,8 +38,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-     func setChart(dataPoints: [String], values: [Double]){
-        
+    func setChart(dataPoints: [String], values: [Double]){
         formato.setValues(values: dataPoints)
         let xaxis:XAxis = XAxis()
 
@@ -53,80 +55,24 @@ class ViewController: UIViewController {
 
         xaxis.valueFormatter = formato
         barView.xAxis.valueFormatter = xaxis.valueFormatter
-        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "Games Played")
+        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "whatever")
         let chartData = BarChartData(dataSets: [chartDataSet])
         chartDataSet.colors = ChartColorTemplates.material()
 
         barView.data = chartData
-
     }
 
+    @IBAction func closeLogs(_ sender: Any) {
+        logView.isHidden = true
+    }
+    
+    @IBAction func saveLogs(_ sender: Any) {
+        logView.isHidden = true
+    }
     
     @IBAction func behaviorLogButton(_ sender: Any) {
-        loadingImage.loadGif(name: "sadGif")
-    }
-    
-    func updateChartData() {
-        // let chartDataSet = PieChartDataSet(values: numberOfDownloadsDataEntries, label: nil)
-        // let chartData = PieChartData(dataSet: chartDataSet)
-        
-        // let colors = [UIColor(named:"iosColor"), UIColor(named:"macColor")]
-        // chartDataSet.colors = colors as! [NSUIColor]
-        
-        // pieChart.data = chartData
-    }
-
-    // TODO: TEST Write Database
-    func testWriteDatabase() {
-
-        // TODO: Test Medication
-        let testMed = Medication(name: "Prozac", dosage: 103.56)
-        uploadMedication(medication: testMed)
-        logMedication(med: testMed, date: "01-18-2020")
-
-        // TODO: Test Symptom
-        let testSymptom = Symptom(name: "tummy-ache", occur: true)
-        uploadSymptom(symptom: testSymptom)
-        logSymptom(sym: testSymptom, date: "01-18-2020")
-
-        // TODO: Test Meal Logging
-        var medArr = [Medication]()
-        let med1 = Medication(name: "Laugh-gas", dosage: 23.4)
-        let med2 = Medication(name: "Mean Girls", dosage: 0.0)
-        medArr.append(med1)
-        medArr.append(med2)
-        var symArr = [Symptom]()
-        let sym1 = Symptom(name: "Hangry", occur: true)
-        symArr.append(sym1)
-        let testMeal = Meal(name: "Pasta", med: medArr, sym: symArr,
-            description: "Pasta was too good for me!")
-        logMeal(meal: testMeal, date: "01-18-2020")
-
-    }
-
-
-
-    // TODO: Test Read Database
-    func testReadDatabase() {
-        
-        // test Medication
-        let med = getMedication(date: "01-18-2020")
-        for med_item in med {
-            print(med_item)
-        }
-
-        // test Symptom
-        let sym = getSymptom(date: "01-18-2020")
-        for sym_item in sym {
-            print(sym_item)
-        }
-
-        // test Meal
-        let meal = getMeal(date: "01-18-2020")
-        for meal_item in meal {
-            print(meal_item)
-        }
-
+        //loadingImage.loadGif(name: "sadGif")
+        logView.isHidden = false
     }
 
 }
